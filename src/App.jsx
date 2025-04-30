2552cfa8ac42fd8bee9fa8c5c1ddb14bb2fea1ec
FOODOHOLIC <adarshkohli183@gmail.com>
2025-04-30 18:52:36 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Dashboard } from "./components/Dashboard";
+import { Landing } from "./components/Landing";
 
 function App() {
-  
   return (
-    <>
-      
-    </>
-  )
-} 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/landing" element={<Landing/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-export default App
+export default App;
