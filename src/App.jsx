09b22ffee2ba8cd35d1f7c0b9ecc93a3486b4813
FOODOHOLIC <adarshkohli183@gmail.com>
2025-05-06 20:50:02 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from "react";
 import "./App.css";
 import { CountContext } from "./context";
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { countAtom } from "./store/atoms/count";
 // import { Dashboard } from "./components/Dashboard";
 const Dashboard = React.lazy(() => import("./components/Dashboard"));
 const Landing = React.lazy(() => import("./components/Landing"));
@@ -84,34 +86,86 @@ const Landing = React.lazy(() => import("./components/Landing"));
 //   );
 // }
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
+//   //warp anyone that wants to use the teleported value inside the provider
+//   return (
+//     <div>
+//       <CountContext.Provider value={count}>
+//       <Count setCount={setCount} />
+//       </CountContext.Provider>
+//     </div>
+//   );
+// }
+
+// function Count({ setCount }) {
+//   return (
+//     <div>
+//       <CountRenderer />
+//       <Buttons setCount={setCount} />
+//     </div>
+//   );
+// }
+
+// function CountRenderer() {
+//   const count = useContext(CountContext);
+//   return <div>{count}</div>;
+// }
+
+// function Buttons({ count, setCount }) {
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}
+//       >
+//         Increasing
+//       </button>
+
+//       <button
+//         onClick={() => {
+//           setCount(count - 1);
+//         }}
+//       >
+//         Decreasing
+//       </button>
+//     </div>
+//   );
+// }
+
+//STATE MANAGEMENT- RECOIL
+
+function App() {
   //warp anyone that wants to use the teleported value inside the provider
   return (
     <div>
-      <CountContext.Provider value={count}>
-      <Count count={count} setCount={setCount} />
-      </CountContext.Provider>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
     </div>
   );
 }
 
-function Count({ setCount }) {
+function Count() {
   return (
     <div>
       <CountRenderer />
-      <Buttons setCount={setCount} />
+      <Buttons />
     </div>
   );
 }
 
 function CountRenderer() {
-  const count = useContext(CountContext);
+  const count = useRecoilValue(countAtom);
+
   return <div>{count}</div>;
 }
 
-function Buttons({ count, setCount }) {
+function Buttons() {
+  const [count, setCount] = useRecoilState(countAtom);
+
   return (
     <div>
       <button
